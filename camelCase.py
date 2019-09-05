7eeaf3931241c1aff6a4becb5d2cb6d7a8fd109e
IsmaelFarah101 <ismaelomermohamed1@gmail.com>
2019-09-05 18:56:31 -0500

    added a banner

--- camelCase.py
+++ camelCase.py
@@ -1,9 +1,21 @@
 import string
-sentence = input('Enter a clunk sentence: ')
-#fetches user input and using capwords to capitlize the first letter of each word and lower case the rest
-newsentence = string.capwords(sentence).replace(" ","")
-## this lowercases the first letter of the word
-newsentence = newsentence[0].lower() + newsentence[1:]
+def camelCase(sentence):
+    #fetches user input and using capwords to capitlize the first letter of each word and lower case the rest
+    newsentence = string.capwords(sentence).replace(" ","")
+    ## this lowercases the first letter of the word
+    newsentence = newsentence[0].lower() + newsentence[1:]
 
-print(newsentence)
+    print(newsentence)
 
+def banner():
+    message = 'Camel Case Generator'
+    slashes = '/' * 20
+    print(f'\n {slashes}\n{message}\n{slashes}')
+
+def main():
+    banner()
+    userInput = input('Enter a clunk sentence: ')
+    print(camelCase(userInput))
+
+if __name__ == '__main__':
+    main()
